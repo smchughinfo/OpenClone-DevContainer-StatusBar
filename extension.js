6ddec6d33aab8604acefec7334d863dbc4bba14e
smchughinfo <seanmchugh.info@gmail.com>
2024-12-02 14:47:47 -0500

    POS WORKS

--- extension.js
+++ extension.js
@@ -1,36 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    // Create a status bar item
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    statusBarItem.text = "THIS IS A TEST";
+    statusBarItem.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "openclone-devcontainer-statusbar" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('openclone-devcontainer-statusbar.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from OpenClone-DevContainer-StatusBar!');
-	});
-
-	context.subscriptions.push(disposable);
+    // Add the status bar item to the context's subscriptions
+    context.subscriptions.push(statusBarItem);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
